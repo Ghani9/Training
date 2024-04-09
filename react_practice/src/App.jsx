e3cfb4ba23e5dfd252966d9ebf5fd5727693e0b9
abdul ghani <abdul.ghanee.larik@gmail.com>
2024-04-09 17:17:54 +0500

    adding react file

---
+++ react_practice/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import HelloWorld from './components/HelloWorld'
+import ChildA from './components/props/ChildA'
+import Counter from './components/Counter'
+import RandomColors from './components/RandomColors'
+
+function App() {
+
+  return (
+    <>
+    {/* <HelloWorld /> */}
+    {/* <ChildA name="Abdul Ghani" /> */}
+    {/* <Counter /> */}
+    {/* <RandomColors /> */}
+    
+    </>
+  )
+}
+
+export default App
---
+++ react_practice/src/components/props/ChildC.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ChildC(props) {
+  return (
+    <div>
+      <h1 className='center'>
+        {props.name}
+      </h1>
+    </div>
+  )
+}
+
+export default ChildC
